c7e3cd018405e4d47d861331b97d1b8d514ce36e
deeptirana <deeptirana10@gmail.com>
2021-11-24 13:28:44 -0800

    added two pointer approach

--- reverse_str.py
+++ reverse_str.py
@@ -11,7 +11,21 @@ def test_palindrome(test):
         return True
     else:
         return False    
+    
+    
+#two Pointer approach
+def test_palindrome(test):
+    i = 0
+    j=len(test)-1
 
+    while(i<j):
+        if test[i] != test[j]:
+            return False
+
+        i+=1
+        j-=1    
+
+    return True        
 
 
 test = 'abba'
