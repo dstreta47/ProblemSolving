6ce9e021e660b1db918c92ebd939e8945df6e3f0
deeptirana <deeptirana10@gmail.com>
2021-11-27 02:21:52 -0800

    longest_sub

---
+++ longest_sub.py
@@ -0,0 +1,15 @@
+def longest_sub(s):
+    seen = set()
+    l=0
+    res = 0
+    for r in range(len(s)):
+        while s[r] in seen:
+            seen.remove(s[l])
+            l+=1
+
+        seen.add(s[r])
+        res = max(res,r-l+1)
+    return res     
+
+
+print(longest_sub('abcabcbb'))       
